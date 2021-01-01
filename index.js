52a4b2ef8fa2f9409f54afd535c6e9c7372e9633
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-01 15:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 document.getElementById('main').remove();
 let newHeader = document.createElement('h1');
-newHeader.id
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "sallyyyy is the champion";
